457562d6d6469e7ae260af48049813cfbe53c0eb
1111Nikhil <nikhilkumarvr1999@gmail.com>
2024-04-30 22:31:47 +0530

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
             src={Menu}
             onClick={HandelMenu}/>
             <img className='h-10 px-2'
-            alt='Logo12'
+            alt='Logo'
             src={Logo}/>
         </div>
         <div className='flex'>
